b4d48dd9331e0e5e06753b0ae3b462bfc56755d1
Andrew Andresen <aandresen@grantham.edu>
2021-05-03 15:26:05 -0500

    First Commit

--- GHv2/Controllers/HomeController.cs
+++ GHv2/Controllers/HomeController.cs
@@ -5,31 +5,123 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GHv2.Models;
+using Firebase.Database.Query;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace GHv2.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+
+
+
+        public IActionResult Index() { return View(); }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(string currentUser)
         {
+            //download from firebase
+            var client2 = new Firebase.Database.FirebaseClient("https://clddb-a63b0-default-rtdb.firebaseio.com/");
+
+            //Retrieve data from Firebase
+            var nameList = new List<string>();
+            var locList = new List<string>();
+            var inList = new List<string>();
+            var outList = new List<string>();
+            var UserList = new List<string>();
+
+            if (currentUser == null)
+            {
+                var dbPlants = await client2
+              .Child("Garden")
+              .Child("default")
+              .OnceAsync<InfoContext>();
+                
+                //Convert JSON data to original datatype
+                foreach (var i in dbPlants)
+                {
+                    nameList.Add(i.Object.Name);
+                    locList.Add(i.Object.Location);
+                    inList.Add(i.Object.DateIn);
+                    outList.Add(i.Object.DateOut);
+                    UserList.Add(i.Object.CurrentUser);
+                }
+            }
+            else
+            {
+                var dbPlants = await client2
+              .Child("Garden")
+              .Child(currentUser)
+              .OnceAsync<InfoContext>();
+                
+                //Convert JSON data to original datatype
+                foreach (var i in dbPlants)
+                {
+                    nameList.Add(JsonConvert.ToString(i.Object.Name).ToString().Trim('"'));
+                    locList.Add(JsonConvert.ToString(i.Object.Location).ToString().Trim('"'));
+                    inList.Add(JsonConvert.ToString(i.Object.DateIn).ToString().Trim('"'));
+                    outList.Add(JsonConvert.ToString(i.Object.DateOut).ToString().Trim('"'));
+                    UserList.Add(JsonConvert.ToString(i.Object.CurrentUser).ToString().Trim('"'));
+                }
+            }
+
+            ViewBag.NameList = nameList;
+            ViewBag.LocList = locList;
+            ViewBag.InList = inList;
+            ViewBag.OutList = outList;
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
 
+        public IActionResult About() { return View(); }
+
+        [HttpPost]
+        public async Task<ActionResult> About(string name, string location, string dateIn, string dateOut, string currentUser)
+        {
+            // upload to firebase <----this works
+            var currentTestData = new InfoContext()
+            {
+                Name = name,
+                Location = location,
+                DateIn = dateIn,
+                DateOut = dateOut,
+                CurrentUser = currentUser
+            };
+            var client = new Firebase.Database.FirebaseClient("https://clddb-a63b0-default-rtdb.firebaseio.com/");
+            await client
+              .Child("Garden")
+              .Child(currentUser)
+              .PostAsync(currentTestData);
+            
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Remove()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Remove(string plantName, string currentUser)
+        {
+           
+            var del = new Firebase.Database.FirebaseClient("https://clddb-a63b0-default-rtdb.firebaseio.com/");
+            await del.Child("Garden")
+                .Child(currentUser)
+                .DeleteAsync();
+             
+            return View();
+        }
+
+        
+        public IActionResult Privacy( )
         {
             return View();
         }
---
+++ GHv2/Models/InfoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GHv2.Models
+{
+    public class InfoContext
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string DateIn { get; set; }
+        public string DateOut { get; set; }
+        public string CurrentUser { get; set; }
+
+    }
+}
